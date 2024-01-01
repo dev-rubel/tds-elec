db51ff30a784ccc80f150f54ba9950f77399e10b
dev-rubel <dpirubel@gmail.com>
2024-01-01 19:24:03 +0600

    update src path

--- src/components/details/Section1/CountNumbers.jsx
+++ src/components/details/Section1/CountNumbers.jsx
@@ -5,9 +5,9 @@ function CountNumbers({selectedSeat}) {
 	return (
 		<div className="grid-x grid-margin-x">
 			<div className="cell medium-12 large-12 small-12" >
-				<CountNumberEach icon={'/sites/all/modules/custom/rsi_election/widgets/election2024/bd-svg/top-total-section/total_voters2.png'} count={selectedSeat.voter} label={'Total Voters'} />
-				<CountNumberEach icon={'/sites/all/modules/custom/rsi_election/widgets/election2024/bd-svg/top-total-section/total_voters2.png'} count={selectedSeat.male_voter} label={'Male Voters'} />
-				<CountNumberEach icon={'/sites/all/modules/custom/rsi_election/widgets/election2024/bd-svg/top-total-section/total_voters2.png'} count={selectedSeat.female_voter} label={'Female Voters'} />
+				<CountNumberEach icon={'/sites/all/modules/custom/rsi_election/widgets/election2024/top-total-section/total_voters2.png'} count={selectedSeat.voter} label={'Total Voters'} />
+				<CountNumberEach icon={'/sites/all/modules/custom/rsi_election/widgets/election2024/top-total-section/total_voters2.png'} count={selectedSeat.male_voter} label={'Male Voters'} />
+				<CountNumberEach icon={'/sites/all/modules/custom/rsi_election/widgets/election2024/top-total-section/total_voters2.png'} count={selectedSeat.female_voter} label={'Female Voters'} />
 				{/* <CountNumberEach icon={'icon'} count={selectedSeat._voter} label={'Seats'} /> */}
 			</div>
 		</div>
