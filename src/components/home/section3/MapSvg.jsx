04b9077754938bc7f873f9a9ebcdf3722676a26a
dev-rubel <dpirubel@gmail.com>
2024-01-02 13:46:56 +0600

    fix allowFullScreen react attribute

--- src/components/home/section3/MapSvg.jsx
+++ src/components/home/section3/MapSvg.jsx
@@ -3,6 +3,6 @@ import { useRef, useState, useEffect } from 'react';
 export default function MapSvg() {
 
   return (
-    <iframe src="https://datawrapper.dwcdn.net/D9POq/4/" allowfullscreen style={{width: '100%', height: '100%'}}></iframe>
+    <iframe src="https://datawrapper.dwcdn.net/D9POq/4/" allowFullScreen style={{width: '100%', height: '100%'}}></iframe>
   );
 }
