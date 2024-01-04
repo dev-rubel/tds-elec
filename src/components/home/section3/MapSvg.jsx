322ed79f9fabf54f33ab4702b9a9c47289879b75
im-nazmul <nazmulhossainshanto@gmail.com>
2024-01-04 15:05:43 +0600

    Mobile banner, Map info, total candidates, and total parties count have been updated.

--- src/components/home/section3/MapSvg.jsx
+++ src/components/home/section3/MapSvg.jsx
@@ -4,11 +4,11 @@ export default function MapSvg() {
   return (
     <>
       <div className="tip-info">
-        <h4>Over 1900 candidates. 28 out of 44 registered political parties</h4>
+        <h4>Over 1900 candidates from 28 out of 44 registered political parties</h4>
         <p>
           <span>But the main fight is within just one party: </span>Awami
           League-nominated candidates--with their Boat insignia--and Awami
-          League-sponsored independents--mostly bearing Eagle and Truck To
+          League-sponsored independents--mostly bearing Eagle and Truck to
           increase voter turnout, the AL high-command allowed party members to
           contest the polls as independents--and as many as 382 AL independents
           are running for 221 seats out of 300.
